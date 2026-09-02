00ac3cf15d4eec8373adb894ee5aa86de39c5244
danjeffrey <5252137+danjeffrey@users.noreply.github.com>
2026-09-02 10:18:54 -0700

    Completed testing module retroactively.

---
+++ main.js
@@ -0,0 +1,58 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => {
+  // webpackBootstrap
+  /******/ "use strict";
+  /******/ var __webpack_modules__ = {
+    /***/ "./src/index.js"(
+      /*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+      __unused_webpack___webpack_module__,
+      __webpack_exports__,
+      __webpack_require__,
+    ) {
+      eval(
+        "{__webpack_require__.r(__webpack_exports__);\n// src/index.js\n\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMiLCJtYXBwaW5ncyI6IjtBQUFBO0FBQ2EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9iYXR0bGVzaGlwLy4vc3JjL2luZGV4LmpzP2U5NzMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gc3JjL2luZGV4LmpzXG5cInVzZSBzdHJpY3RcIjtcblxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/index.js\n\n}",
+      );
+
+      /***/
+    },
+
+    /******/
+  };
+  /************************************************************************/
+  /******/ // The require scope
+  /******/ const __webpack_require__ = {};
+  /******/
+  /************************************************************************/
+  /******/ /* webpack/runtime/make namespace object */
+  /******/ // define __esModule on exports
+  /******/ __webpack_require__.r = (exports) => {
+    /******/ Object.defineProperty(exports, Symbol.toStringTag, {
+      value: "Module",
+    });
+    /******/ Object.defineProperty(exports, "__esModule", { value: true });
+    /******/
+  };
+  /******/
+  /************************************************************************/
+  /******/
+  /******/ // startup
+  /******/ // Load entry module and return exports
+  /******/ // This entry module can't be inlined because the eval-source-map devtool is used.
+  /******/ let __webpack_exports__ = {};
+  /******/ __webpack_modules__["./src/index.js"](
+    0,
+    __webpack_exports__,
+    __webpack_require__,
+  );
+  /******/
+  /******/
+})();
